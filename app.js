fc42bed6cc4ce4374bad0e3375083293a166dc1d
Julia Brauer <julia.brauer83@gmail.com>
2023-07-15 22:39:30 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-let output = document.querySelector('#outputScreen'); 
-let currentBalance =  Math.floor(Math.random() * 5000) + 1;
 
-function checkBalance() {
-    output.innerHTML = "Your current balance is: $" + currentBalance;
+
+class ATM {
+	currentBalance =  Math.floor(Math.random() * 5000) + 1;
+
+	output(str) {
+		let outputElement = document.querySelector('#outputScreen'); 
+		outputElement.innerHTML = str;
+	}
+
+	checkBalance() {
+		//this.output.innerHTML = `Your current balance is:<br>$${this.currentBalance}`;
+		this.output(`Your current balance is:<br>$${this.currentBalance}`)
+	}
+
+	makeDeposit() {
+		let depositAmount = Math.floor(Math.random() * 1000) + 1;
+		this.currentBalance += depositAmount;
+		this.output(`Deposit: $${depositAmount}<p>Your new balance is:<br>$${this.currentBalance}</p>`);
+	}
+
+	makeWithdrawal() {
+		let withdrawalAmount = Math.floor(Math.random() * 500) + 1;
+
+		if(withdrawalAmount > this.currentBalance) {
+			this.output(`Attempted Withdrawal: $${withdrawalAmount}
+						<p>Your current balance is:<br>$${this.currentBalance}</p>`);
+		} else {
+			this.currentBalance -= withdrawalAmount;
+			this.output(`Withdrawal: $${withdrawalAmount}
+						<p>Your new balance is:<br>$${this.currentBalance}</p>`);
+		}
+	} 
+
+	exit() {
+		this.output("WELCOME");
+	}
 }
 
-function makeDeposit() {
-    let depositAmount = Math.floor(Math.random() * 1000) + 1;
-    currentBalance += depositAmount;
-    output.innerHTML = "You deposited $" + depositAmount + 
-        "<br>Your new balance is: $" + currentBalance;
-}
+let myATM = new ATM();
 
-function makeWithdrawal() {
-    let withdrawalAmount = Math.floor(Math.random() * 500) + 1;
-
-    if(withdrawalAmount > currentBalance) {
-        output.innerHTML = "No can do" +
-            "<br>You attempted to withdraw $" + withdrawalAmount +
-            "<br>Your current balance is: $" + currentBalance;
-    } else {
-        currentBalance -= withdrawalAmount;
-        output.innerHTML = "You withdrew $" + withdrawalAmount + 
-            "<br>Your new balance is: $" + currentBalance;
-    }
-} 
-
-function exit() {
-    output.innerHTML = "WELCOME";
-}
